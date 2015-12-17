6304620f803034a69902e928fae1479a60b22b2b
Chuck Steel <chuck@steelfarms.org>
2015-12-16 22:01:47 -0500

    Enable Remote Desktop
    Enables remote desktop to computers and turns off the Network Level
    Authentication requirement

---
+++ EnableRemoteDesktop.cmd
@@ -0,0 +1,2 @@
+reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication  /t REG_DWORD /d 0 /f
